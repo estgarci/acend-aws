3df2e723791826041c9e1e3721fbe7742fd75b19
esteban garcia <egarci@fastmail.com>
2021-12-16 12:09:17 -0700

    delete comments

--- client/src/components/FightNav/FlightDisplay.js
+++ client/src/components/FightNav/FlightDisplay.js
@@ -34,26 +34,10 @@ function RenderFlight({flight}) {
                         <Button className='bg-secondary'>Book flight</Button>
                         
                     </Col>
-                    {/* <Col xs={12}>
-                        <Row className='justify-content-center'></Row>
-                        <Row xs={12} className='justify-content-center'>Operatred by {flight.operator} </Row>
-                        
-                    </Col> */}
                 </Row>
-                {/* <Row xs={12} className='justify-content-center'>
-                    <Col>
-                        <Row xs={12} className='justify-content-center'>Operatred by {flight.operator} </Row>
-                        <Row xs={12} className='justify-content-center'><small><a href='#'>Details</a></small></Row>
-                    </Col>
-                </Row> */}
+            
             </div>
-        // <tr id={flight.fa_flight_id}>
-        //     <td>{flight.operator}</td>
-        //     <td>{flight.origin.code}</td>
-        //     <td>{flight.destination.code}</td>
-        //     <td>{departureDate}</td>
-        //     <td>{arrivalDate}</td>
-        // </tr>
+   
     );
 }
 
@@ -93,34 +77,6 @@ function FlightDisplay(props) {
     );
 }
 
-
-
-// function testCard(props){
-//     return(
-//         <div className='container bg-primary m-3'>
-//             <div className="container p-3 bg-white justify-content-center ">
-//                 <Row xs={12} >
-//                     <Col xs={6} className=''>
-//                         <Row className="justify-content-center h5">LAX&nbsp;<svg class="DateRangePickerInput_arrow_svg DateRangePickerInput_arrow_svg_1" focusable="false" viewBox="0 0 1000 1000"><path d="M694 242l249 250c12 11 12 21 1 32L694 773c-5 5-10 7-16 7s-11-2-16-7c-11-11-11-21 0-32l210-210H68c-13 0-23-10-23-23s10-23 23-23h806L662 275c-21-22 11-54 32-33z"></path></svg>&nbsp;PHX</Row>
-//                         <Row className='justify-content-center h4'>departure  arrival</Row>
-//                         <Row className='justify-content-center h5'>distance</Row>
-//                     </Col>
-//                     <Col xs={6}>
-//                         <Row className='justify-content-center h4'>Trip from</Row>
-//                         <Row className='justify-content-center h4'>$777</Row>
-//                         <Row className='justify-content-center'></Row>
-//                     </Col>
-//                 </Row>
-//                 <Row xs={12} className='justify-content-center'>
-//                     <Col>
-//                         <Row xs={12} className='justify-content-center'>Operatred by AIRLNE </Row>
-//                         <Row xs={12} className='justify-content-center'><small><a href='#'>Details</a></small></Row>
-//                     </Col>
-//                 </Row>
-//             </div>
-//         </div>
-//     )
-// }
 
 
 
