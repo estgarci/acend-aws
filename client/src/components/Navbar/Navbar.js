526d9b1542b6b87408cff3669d5740d88233afd2
esteban garcia <egarci@fastmail.com>
2022-02-23 12:50:43 -0700

    changed github react component redirect uri to default, none

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -60,7 +60,6 @@ export function ContWithGithub(props) {
         size='medium'
         onSuccess={responseGit}
         className='github-login'
-        redirectUri='https://www.acend.site/api/users/github/token'
   />
   </>
   )
