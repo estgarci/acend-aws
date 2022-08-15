639cc3578c0417e32108e1d832a753214fe5243b
esteban garcia <egarci@fastmail.com>
2022-08-15 07:44:36 -0700

    changed clientURL in config.js file

--- server/config.js
+++ server/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/acend',
-    'clientURL': 'http:localhost:3001',
+    // 'clientURL': 'http:localhost:3001',
+    'clientURL': 'https://www.acend.site',
     'facebook': {
         clientId: '1369933030105210',
         clientSecret: 'ada1dec1a5e6dc772e68d2d558f9e889'
