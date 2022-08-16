0a7a41ef75a5aaac34f3b9a1887c2dffd0c4015c
esteban garcia <egarci@fastmail.com>
2022-08-16 12:46:46 -0700

    modified pop up message component

--- client/src/shared/baseUrl.js
+++ client/src/shared/baseUrl.js
@@ -1,2 +1,2 @@
-// export const baseUrl = 'https://acend.site/';
-export const baseUrl = 'https://localhost:3443/';
+export const baseUrl = 'https://acend.site/';
+// export const baseUrl = 'https://localhost:3443/';
